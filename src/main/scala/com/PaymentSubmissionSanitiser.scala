7f0729ed7e8a61ca77a0107e57e8215e950cab5f
Cosimo Ranieri <cosimoranieri@cosimos-mbp.home>
2018-11-04 18:59:51 +0000

    use EitherT for each function

--- src/main/scala/com/PaymentSubmissionSanitiser.scala
+++ src/main/scala/com/PaymentSubmissionSanitiser.scala
@@ -2,18 +2,24 @@ package com
 
 import java.time.LocalDateTime
 
+import cats.data.EitherT
 import com.model._
 import com.model.cycles._
+import com.model.statuses.{NotSanitised, PaymentStatus}
+
+import scala.concurrent.{ExecutionContext, Future}
+import cats.implicits._
 
 trait PaymentSubmissionSanitiser {
-
-  def sanitise: UnvalidatedPaymentSubmission => CycleResponseType[NotSanitisedPaymentSubmission, SanitisedPaymentSubmission] = {
+  import cats.instances.future._
+  def sanitise(unvalidatedPaymentSubmission: UnvalidatedPaymentSubmission)(implicit ec: ExecutionContext): EitherT[Future, _ <: PaymentSubmissionWithStatus[_ >: PaymentStatus], SanitisedPaymentSubmission] =
+    unvalidatedPaymentSubmission match {
     case UnvalidatedPaymentSubmission(paymentSub) if paymentSub.reference == "ref" =>
       println(s"ref: ${paymentSub.reference}")
-      Right(SanitisedPaymentSubmission(paymentSub))
+      EitherT.right( Future { SanitisedPaymentSubmission(paymentSub) })
     case UnvalidatedPaymentSubmission(paymentSub)  =>
       val value = PaymentSubmissionValue(paymentSub.amount, paymentSub.reference, Some(LocalDateTime.now()))
-      Left(NotSanitisedPaymentSubmission(value))
+      EitherT.left(Future { NotSanitisedPaymentSubmission(value)}).leftWiden[PaymentSubmissionWithStatus[_ >: PaymentStatus]]
   }
 }
 
