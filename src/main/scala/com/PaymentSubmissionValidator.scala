dc12326fd3841f06e94d1421f9d2ed211d6adc77
Cosimo Ranieri <cosimoranieri@cosimos-mbp.home>
2018-11-04 19:01:21 +0000

    change the validator to return EitherT

--- src/main/scala/com/PaymentSubmissionValidator.scala
+++ src/main/scala/com/PaymentSubmissionValidator.scala
@@ -2,20 +2,28 @@ package com
 
 import java.time.LocalDateTime
 
-import com.model._
-import com.model.cycles._
 
+import com.model._
+import com.model.cycles.{PaymentSubmissionWithStatus, _}
+
+import scala.concurrent.{ExecutionContext, Future}
+//import cats.instances.future._
+import cats.data._
+import cats.implicits._
+import com.model.statuses.{PaymentStatus}
 trait PaymentSubmissionValidator {
 
   val amountLimit = 1000
 
-  def validate: PartialFunction[SanitisedPaymentSubmission, CycleResponseType[InvalidPaymentSubmission, ValidPaymentSubmission]] = {
+  def validate(sanitisedPaymentSubmission: SanitisedPaymentSubmission)(implicit ec: ExecutionContext): EitherT[Future, PaymentSubmissionWithStatus[_ >: com.model.statuses.PaymentStatus], ValidPaymentSubmission] =
+    sanitisedPaymentSubmission match {
     case SanitisedPaymentSubmission(paymentSubmission) =>
       if (paymentSubmission.amount < amountLimit)
-        Right(ValidPaymentSubmission(paymentSubmission))
+        EitherT.right(Future { ValidPaymentSubmission(paymentSubmission) })
       else {
         val value = PaymentSubmissionValue(paymentSubmission.amount, paymentSubmission.reference, Some(LocalDateTime.now()))
-        Left(InvalidPaymentSubmission(value))
+        val e = EitherT.leftT[Future, ValidPaymentSubmission]( InvalidPaymentSubmission(value))
+        e.leftWiden[PaymentSubmissionWithStatus[_ >: PaymentStatus]]
       }
   }
 }
